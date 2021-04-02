91727eb6cdf2a34bc2471914f8e47108fd6cb303
ElizabethCowell <80732868+ElizabethCowell@users.noreply.github.com>
2021-04-02 14:18:59 -0400

    circle solution

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace AreaOfCircle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is the radius of your circle?");
+            var radius = double.Parse(Console.ReadLine());
+            double AreaOfCircle = (Math.PI * radius * radius);
+            Console.WriteLine($"The area of a circle with the radios of {radius} is {AreaOfCircle}");
+        }
+    }
+}
